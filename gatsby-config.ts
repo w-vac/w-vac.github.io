03a80002585caebce54be7a1b876221695976fbf
w-vac <psyfroz@gmail.com>
2024-09-14 13:08:22 -0400

    changed metadata

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -8,9 +8,9 @@ const config: GatsbyConfig = {
     // You can overwrite values here that are used for the SEO component
     // You can also add new values here to query them like usual
     // See all options: https://github.com/LekoArts/gatsby-themes/blob/main/themes/gatsby-theme-cara/gatsby-config.mjs
-    siteTitle: `Cara`,
-    siteTitleAlt: `Cara - Gatsby Starter Portfolio`,
-    siteHeadline: `Cara - Gatsby Theme from @lekoarts`,
+    siteTitle: `Will`,
+    siteTitleAlt: `Will Canner - Portfolio`,
+    siteHeadline: `Will Canner - Portfolio`,
     siteUrl: `https://cara.lekoarts.de`,
     siteDescription: `Playful and Colorful One-Page portfolio featuring Parallax effects and animations`,
     siteImage: `/banner.jpg`,
